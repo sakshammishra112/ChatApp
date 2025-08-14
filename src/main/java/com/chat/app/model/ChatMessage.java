cf77be63bb86ff7b3502d5706104f5f05e208224
Saksham Mishra <158152958+sakshammishra112@users.noreply.github.com>
2025-08-14 20:36:24 +0530

    Update ChatMessage.java

--- src/main/java/com/chat/app/model/ChatMessage.java
+++ src/main/java/com/chat/app/model/ChatMessage.java
@@ -8,8 +8,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ChatMessage {
     private Long id;
     private String sender;
